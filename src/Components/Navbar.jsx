a14f2cf21623a226f643ecf9017e6491824a8511
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-04-19 15:24:56 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,6 @@
+{/* <nav className="bg-gray-900 text-white p-4 flex justify-center gap-6">
+  <Link to="/" className="hover:text-yellow-400">Menu</Link>
+  <Link to="/cart" className="hover:text-yellow-400">Cart</Link>
+  <Link to="/order" className="hover:text-yellow-400">Order</Link>
+  <Link to="/history" className="hover:text-yellow-400">History</Link>
+</nav> */}
